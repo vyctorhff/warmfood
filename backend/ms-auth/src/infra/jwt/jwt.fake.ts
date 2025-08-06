11322f36cc6ea7b25c01d8cf159e2ec8c4521c4e
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2025-08-06 11:33:59 -0300

    backend: ms-auth: update jwt fake to use promise

--- backend/ms-auth/src/infra/jwt/jwt.fake.ts
+++ backend/ms-auth/src/infra/jwt/jwt.fake.ts
@@ -1,13 +1,13 @@
 import { Account } from "../../core/domain/acount.domain.ts";
-import { LoginResposne } from "../../core/domain/login.domain.ts";
+import { LoginResponse } from "../../core/domain/login.domain.ts";
 import { EncryptUser } from "../../core/usercase/user.security.ts";
 
-export class JwtFacke implements EncryptUser {
-  encrypt(account: Account): LoginResposne {
-    return {
+export class JwtFake implements EncryptUser {
+  encrypt(account: Account): Promise<LoginResponse> {
+    return Promise.resolve({
         token: 'xxxxxx',
         refresh: 'yyyyy',
         date: new Date()
-    };
+    });
   }
 }
