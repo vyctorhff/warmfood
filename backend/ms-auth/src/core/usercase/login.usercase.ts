6d3df36e615269395df8e2e0d96b4cd5b8c1a30f
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2025-08-06 11:32:56 -0300

    backend:ms-auth: add logger and update interface to accept jwt

--- backend/ms-auth/src/core/usercase/login.usercase.ts
+++ backend/ms-auth/src/core/usercase/login.usercase.ts
@@ -1,5 +1,6 @@
-import { LoginRequest, LoginResposne } from "../domain/login.domain.ts";
+import { LoginRequest, LoginResponse } from "../domain/login.domain.ts";
 import { AccountRepositoy } from "./data.repository.ts";
+import { Logger } from "./logger.ts";
 import { EncryptUser } from "./user.security.ts";
 
 export class LoginUserCase {
@@ -7,19 +8,24 @@ export class LoginUserCase {
     constructor(
         private accountRepositoy: AccountRepositoy,
         private encryptUser: EncryptUser,
+        private log: Logger
     ) {}
 
-    login(loginRequest: LoginRequest): LoginResposne {
+    async login(loginRequest: LoginRequest): Promise<LoginResponse> {
+        this.log.info('Validating user');
+
         const { login, pass } = loginRequest;
         if (!login || !pass) {
             throw new Error('Login or pass invalid');
         }
+
+        this.log.info('Creating user security');
 
         const account = this.accountRepositoy.finByLoginAndPass(login, pass);
         if (!account) {
             throw new Error('Account not found');
         }
 
-        return this.encryptUser.encrypt(account);
+        return await this.encryptUser.encrypt(account);
     }
 }
--- backend/ms-auth/src/core/usercase/user.security.ts
+++ backend/ms-auth/src/core/usercase/user.security.ts
@@ -1,6 +1,6 @@
 import { Account } from "../domain/acount.domain.ts";
-import { LoginResposne } from "../domain/login.domain.ts";
+import { LoginResponse } from "../domain/login.domain.ts";
 
 export interface EncryptUser {
-    encrypt(account: Account): LoginResposne;
+    encrypt(account: Account): Promise<LoginResponse>;
 }
