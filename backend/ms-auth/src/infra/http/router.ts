dff9810966952e6e9ab8c8cadac4d424717224fc
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2025-08-12 11:50:18 -0300

    backend:ms-auth: refact route for health and login

--- backend/ms-auth/src/infra/http/router.ts
+++ backend/ms-auth/src/infra/http/router.ts
@@ -1,6 +1,7 @@
 import { Router, Context } from "@oak/oak";
 import { Status } from "jsr:@oak/commons@1/status";
 import { createLoginController } from "./controllers/login.controller.ts";
+import { createHealthController } from "./controllers/health.controller.ts";
 
 const router = new Router();
 
@@ -11,15 +12,22 @@ export const create = () => {
 // ###############################################################################
 // Health
 router.get("/health", (context) => {
-    context.response.body = { status: "UP" };
-    context.response.status = Status.OK;
+  const healthController = createHealthController();
+  const result = healthController.getStatus();
+
+  context.response.body = result;
+  context.response.status = Status.OK;
 });
 
 // ###############################################################################
 // Login
 router.post("/login", async (context: Context) => {
   const loginController = createLoginController();
-  const result = loginController.login(await context.request.body.json());
+  const result = await loginController.login(await context.request.body.json());
   
   context.response.body = result;
+  context.response.status = Status.OK;
 });
+
+// ###############################################################################
+// 
